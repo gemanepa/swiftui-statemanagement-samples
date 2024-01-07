f83e5a3babf7fa30b3e97905790ddf1a039dd12d
gemanepa <contact@gemanepa.com>
2024-01-07 17:42:09 -0300

    Initial Commit

---
+++ StateManagementSamples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StateManagementSamples
+//
+//  Created by Gabriel Ernesto Martinez Canepa on 07/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StateManagementSamples/StateManagementSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateManagementSamplesApp.swift
+//  StateManagementSamples
+//
+//  Created by Gabriel Ernesto Martinez Canepa on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct StateManagementSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
